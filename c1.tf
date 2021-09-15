33236700436aa69142ab0096c55fdaf97bef31a1
abhicse2014 <43094617+abhicse2014@users.noreply.github.com>
2021-09-15 19:09:12 +0530

    Update c1.tf

--- c1.tf
+++ c1.tf
@@ -1,4 +1,10 @@
-resource "aws_vpc" "my_app" {
+resource "aws" {
+  region= "us-east-1"
+  access_key = "AKIAVQNTEG3SAHFVKNGS"
+  secret_key = "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"
+}
+
+resource " "my_app" {
   cidr_block       = var.vpc_cidr
   instance_tenancy = "default"
 
