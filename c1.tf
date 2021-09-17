e86e24947cb83c0f3b2e6a2457732d13abb8f978
abhicse2014 <43094617+abhicse2014@users.noreply.github.com>
2021-09-17 09:17:07 +0530

    Update c1.tf

--- c1.tf
+++ c1.tf
@@ -1,9 +1,19 @@
+terraform {
+  backend "s3" {
+    bucket = "mybucket"
+    key    = "terraform"
+    region = "us-east-1"
+  }
+}
+
+
 provider "aws" {
   region = "us-east-1"  
    shared_credentials_file = "/credentials"
     profile = "default"
-
 }
+
+
 
 
 resource "aws_instance" "web" {
